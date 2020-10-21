9651460efba48d90da90c7c5a081a2273f90a963
Jake Humphreys <h024470h@student.staffs.ac.uk>
2020-10-21 20:58:14 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Discord_Letter_Formatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.lblCharsTitle = new System.Windows.Forms.Label();
+            this.lblCharCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(386, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(12, 39);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(386, 23);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 68);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(386, 141);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.Text = "";
+            // 
+            // lblCharsTitle
+            // 
+            this.lblCharsTitle.AutoSize = true;
+            this.lblCharsTitle.Location = new System.Drawing.Point(13, 216);
+            this.lblCharsTitle.Name = "lblCharsTitle";
+            this.lblCharsTitle.Size = new System.Drawing.Size(61, 13);
+            this.lblCharsTitle.TabIndex = 3;
+            this.lblCharsTitle.Text = "Characters:";
+            // 
+            // lblCharCount
+            // 
+            this.lblCharCount.AutoSize = true;
+            this.lblCharCount.Location = new System.Drawing.Point(71, 216);
+            this.lblCharCount.Name = "lblCharCount";
+            this.lblCharCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCharCount.TabIndex = 4;
+            this.lblCharCount.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 238);
+            this.Controls.Add(this.lblCharCount);
+            this.Controls.Add(this.lblCharsTitle);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Discord Letter Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label lblCharsTitle;
+        private System.Windows.Forms.Label lblCharCount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Letter_Formatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+            string output = "";
+            int count = 0;
+
+            txtOutput.Clear();
+
+            foreach (char c in input)
+            {
+                if (Char.IsLetterOrDigit(c) || c == ' ')
+                {
+                    output = convertToDiscordShorthand(c.ToString());
+                    txtOutput.Text += output + " ";
+                }
+                else
+                {
+                    MessageBox.Show("Discord doesn't support regional indicators for symbols.");
+                    break;
+                }
+                
+            }
+
+            foreach(char c in txtOutput.Text)
+            {
+                count++;
+            }
+
+            lblCharCount.Text = count.ToString();
+
+            if(count > 2000)
+            {
+                lblCharCount.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblCharCount.ForeColor = Color.Green;
+            }
+
+        }
+
+        public string convertToDiscordShorthand(string input)
+        {
+            try
+            {
+                string output = "";
+
+                if(input != "")
+                {
+                    if(input == " ")
+                    {
+                        output = input;
+                        return output;
+                    }
+                    else
+                    {
+                        output = ":regional_indicator_" + input + ":";
+                        return output;
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Please provide text to convert.");
+                    return "";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return "";
+            }
+        }
+    }
+}
